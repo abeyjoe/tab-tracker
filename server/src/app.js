5cf300ed0ad2269785bd0c6198ffcfcc634d8c16
abeyjoe <abeyjoe18@gmail.com>
2022-12-09 07:10:45 -0500

    setup of Express.js server

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
@@ -14,4 +14,4 @@ app.get('/status', (req, res) => {
   })
 })
 
-app.listen(5000)
+app.listen(process.env.PORT || 5000)
